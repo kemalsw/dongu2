03baecbc2febe98d7a9b3bcce4f7dcee4271f171
Ali Kemal Gündüz <kemall.gunduz@icloud.com>
2023-07-17 16:22:53 +0300

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  Console.WriteLine("1 den başlayıp console dan girilen sayıya kadar ortalama hesaplayıp ekrana yazdıran program");
+              Console.WriteLine("Bir sayı giriniz");
+              int sayi = Convert.ToInt32(Console.ReadLine());
+              int sayac = 1;
+              int toplam = 0;
+              int ortalama;
+
+              while (sayac<=sayi)
+              {
+                  toplam += sayac;
+                  sayac++;
+              }
+              ortalama = toplam / sayac;
+              Console.WriteLine(ortalama  );
+              Console.ReadLine();*/
+
+            char karakter = 'a';
+            while (karakter<='z')
+            {
+                Console.WriteLine(karakter);
+                karakter++;
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
